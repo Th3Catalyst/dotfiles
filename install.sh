5a1b28cec2ca4b38f764874d90ab5046d08b7095
Th3Catalyst <max675295@gmail.com>
2025-11-05 16:25:49 -0800

    h

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 install() {
+    cd ~
     mkdir temp
     cd temp
-    git clone
+    git clone "https://github.com/Th3Catalyst/dotfiles.git"
+    echo "Copying files..."
+    scp -vvv ~/temp/dotfiles/.nanorc ~/
+    scp -vvv ~/temp/dotfiles/.bash_profile ~/
+    echo "Copying complete"
+    cd ~
+    echo "Sourcing files..."
+    source ~/.bashrc
+    rm -r temp
 }
